e2cd7332f06a7e03196499d024345067c8da5346
Jared <jaredtaylor@cbrc.net>
2015-04-01 09:06:38 -0400

    Initial Commit

---
+++ Scope/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Scope
+//
+//  Created by Jared Taylor on 4/1/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
